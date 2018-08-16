d535739bf54f14c9bc8a2b3ad9070d368c3a6648
Richard Neumann <r.neumann@homeinfo.de>
2018-08-16 13:03:14 +0200

    Refac.

--- anonstats/wsgi.py
+++ anonstats/wsgi.py
@@ -56,7 +56,7 @@ def list_():
         return ('Invalid date.', 400)
 
     return JSON([
-        anon_stats.to_dict(autofields=False)
+        anon_stats.to_json(autofields=False)
         for anon_stats in _get_stats(start, end)])
 
 
